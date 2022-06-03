bd896b90f5ee4c2c3ecbc712d42ea56325e6ce7e
vcloud <vcloud@docker.vm>
2022-06-03 22:18:53 +0300

    upgrade

--- build.sh
+++ build.sh
@@ -3,5 +3,4 @@
 echo "*************************"
 echo "***BUILDING WAR**********"
 echo "*************************"
-echo $PWD
-docker run --rm -v $PWD:/app -w /app maven:3-alpine jar -cvf sample.war *
+docker run --rm -v /home/vcloud/jenkins/jenkins_home/workspace/tomcat_sample_master:/app -w /app maven:3-alpine jar -cvf sample.war *
