955378829eac0a003e58b89e24946e7ee2a48381
vcloud <vcloud@docker.vm>
2022-06-04 15:55:27 +0300

    update

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "*******************************"
+echo "***Deploying to Kubernetes*****"
+echo "*******************************"
+
+scp -i scp -i ~/.ssh/remote-key-kubernetes publish.sh vcloud@192.168.1.14:/home/vcloud
