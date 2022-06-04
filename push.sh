db14c0e964e40c198b107993b219ccd3c782179f
ibrahimaydemir01 <106836952+ibrahimaydemir01@users.noreply.github.com>
2022-06-04 14:14:02 +0300

    Delete push.sh

--- push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "*************************"
-echo "***Pushing İmage**********"
-echo "*************************"
-docker build -t ibrahimaydemir/tomcat:v$@ .
-docker push ibrahimaydemir/tomcat:v$@
