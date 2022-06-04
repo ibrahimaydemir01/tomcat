957eff5c7c208e3c9a38ddbaad2f3fece9cce0f2
vcloud <vcloud@docker.vm>
2022-06-04 16:55:59 +0300

    ads

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,5 @@ echo "***Deploying to Kubernetes*****"
 echo "*******************************"
 
 scp -i ~/.ssh/remote-key-kubernetes publish.sh vcloud@192.168.1.14:/home/vcloud
+
+ssh -i ~/.ssh/remote-key-kubernetes vcloud@192.168.1.14 'bash -s' < publish.sh $@
--- publish.sh
+++ publish.sh
@@ -3,3 +3,5 @@
 echo "*************************"
 echo "***Publish Website**********"
 echo "*************************"
+
+sed -i -E "s/^appVersion: (.*)/appVersion: $@/" tomcat-helm/Chart.yaml
