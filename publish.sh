7ea858b532349e1fd4c5170ede293f9a24fce9b3
ibrahimaydemir01 <106836952+ibrahimaydemir01@users.noreply.github.com>
2022-06-06 23:13:37 +0300

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -4,6 +4,6 @@ echo "*************************"
 echo "***Publish Website**********"
 echo "*************************"
 
-sed -i -E "s/^appVersion: (.*)/appVersion: v$@/" tomcat-helm/Chart.yaml
-helm upgrade tomcat-helm tomcat-helm/.
+sed -i -E "s/^appVersion: (.*)/appVersion: v$@/" website-prod/Chart.yaml
+helm upgrade website-prod website-prod/.
 helm ls
